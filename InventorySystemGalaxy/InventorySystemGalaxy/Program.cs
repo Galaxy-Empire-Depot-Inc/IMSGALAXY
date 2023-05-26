c41844e251ad35a090ff9ed2610a47a22f25c2dc
GALAXYEMPIRE-IT\Bansanity <iamjude0507@gmail.com>
2023-05-26 17:25:09 +0800

    Merge Conflict fixed

--- InventorySystemGalaxy/InventorySystemGalaxy/Program.cs
+++ InventorySystemGalaxy/InventorySystemGalaxy/Program.cs
@@ -11,11 +11,9 @@ namespace InventorySystemGalaxy
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-<<<<<<< HEAD
+
             Application.Run(new SplashScreen());
-=======
-            Application.Run(new WelcomeMessageForm());
->>>>>>> 4dbb8c15b58cf595efcd2b139a6f0264aadd6b7d
+
         }
     }
 }
